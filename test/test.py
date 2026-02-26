53737148e1ca59ae4f7c6ae3b88fb720506d4f14
Leonardo Cofone <leonardo.cofone5@gmail.com>
2026-02-26 16:43:28 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,18 +6,17 @@ load_dotenv()
 link = "https://leonardocofone.github.io/Leonardo_Cofone/"
 
 rag = RAG(
-    model_provider="openrouter",
-    model="arcee-ai/trinity-large-preview:free",
+    model_provider="openrouter",                                  # 18 providers + local
+    model="arcee-ai/trinity-large-preview:free",                  # all the model for all providers
     model_api_key=os.getenv("OPEN"),
 
-    faiss=True,
-    embedding_provider="local",
+    faiss=True,            
+    embedding_provider="local",                                   # 10 options/providers
+    embedding_model="sentence-transformers/all-MiniLM-L6-v2",     # If it's not installet yet, it will be installed
 
-    chunk_mode="smart",
-    persist_path="./my_db",
-    memory=True
-    
-).add_source(link).add_source("docs/")
+    persist_path="./my_db",                                       # path to persist db
+    memory=True,                                                  # memory for conversation
+).add_source(link).add_source("docs_ex/").add_source("note_ex.txt")
 
 while True:
     print("\n--------------------------------------------------\n")
